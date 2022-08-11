8b5d9be9a3b9079566af4bd1dc463f16319b2f28
Andy Dziabo <jameszabo@yahoo.com>
2022-08-11 18:13:07 -0400

    commit tree cf62ca6ff0b7a4007f4cc56e5f72f45f01b879e4

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    const newCards = [];
+    for(let i=0; i < array.length; i++){
+        newCards[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`; 
+    }
+    return newCards;
+}
+function countDown(num){
+    let i = num;
+    while(0 <= i){
+        console.log(num);
+        num = num -1;
+        i--;
+    }
+}
